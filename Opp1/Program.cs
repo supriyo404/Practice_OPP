222a4b52bfbaf70c5c4b42f96bc8ce9f8727d09e
supriyo404 <er.supriya93@gmail.com>
2021-05-27 18:23:13 +0530

    init

--- Opp1/Program.cs
+++ Opp1/Program.cs
@@ -6,7 +6,7 @@ namespace Opp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hello Git");
         }
     }
 }
